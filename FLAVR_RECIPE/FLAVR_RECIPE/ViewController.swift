a492ede6957ee9d580d65eafcb28d126fcd0d8e6
ghayuh <ghayuhfernanda@gmail.com>
2019-06-30 23:23:01 +0700

    Create File

---
+++ FLAVR_RECIPE/FLAVR_RECIPE/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FLAVR_RECIPE
+//
+//  Created by Ghayuh Fernanda Putra on 30/06/19.
+//  Copyright © 2019 Ghayuh Fernanda Putra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
